799c18fabf0b271b6ae158e4477a556bff669f75
AysanurKart <ayka21ab@student.cbs.dk>
2021-12-06 01:19:37 +0100

    færdig med login front-end og gemme bruger

---
+++ source/views/index.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const user = localStorage.getItem("user");
+    if (user) {
+        location.href = "homepage.html";
+    }
+
+    document.getElementById("login_form").addEventListener("submit",(event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+
+        const user = {
+            email: email,
+            password: password,
+        };
+
+        fetch("http://localhost:2000/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            if (response) {
+                localStorage.setItem("user", JSON.stringify(user));
+                location.href = "homepage.html";
+                }else {
+                    window.alert("Wrong Info");
+                }
+        })
+        .catch(() =>{
+            window.alert("error");
+        });
+    });
+});
